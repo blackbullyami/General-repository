2270067cd3bfd5a0e92a51cf1a3d26a27293a575
blackbullyami <mayank.rawat1@s.amity.edu>
2024-01-04 13:56:08 +0530

    changes are welcome

---
+++ class1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+// calculating factorial using recursive approach 
+
+public class class1 {
+    static long facrec(long n){
+        if (n==1 || n==0){
+            return (1);
+        }
+        else {
+            return n*facrec(n-1);
+        }
+    }
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter your number :-");
+        long x=s.nextLong();
+        System.out.println("your factorial of x is:"+" "+facrec(x));
+    }
+    
+    
+}
