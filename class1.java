c174b78624a9f05564aaad18d4e9a2b7335a2205
blackbullyami <mayank.rawat1@s.amity.edu>
2024-01-12 15:25:28 +0530

    my new knowledge

--- class1.java
+++ class1.java
@@ -1,7 +1,8 @@
  import java.util.Scanner;
+ import java.util.ArrayList;
 // calculating factorial using recursive approach 
 
-public class class1 {
+/*public class class1 {
     static long facrec(long n){
         if (n==1 || n==0){
             return (1);
@@ -17,4 +18,23 @@ public class class1 {
         System.out.println("your factorial of x is:"+" "+facrec(x));
     }
 
+}*/
+
+//Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, 
+//between 2000 and 3200 (both included).
+//The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+public class class1{
+    public static void main(String args[]){
+        ArrayList <Integer> t=new ArrayList<Integer>();
+        for (int i=2001;i<3200;i++){
+            if (i%7==0 && i%5!=0){
+               t.add(i); 
+
+            }
+        }
+        for (int x:t){
+            System.out.print(x+",");
+        }
+    }
 }
